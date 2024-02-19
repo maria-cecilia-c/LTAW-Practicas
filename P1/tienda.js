2e279812e663a80133f6c4d8076552564f827816
MCcampos <mc.campos.2020@alumnos.urjc.es>
2024-02-19 10:59:00 +0100

    tiendita

--- P1/tienda.js
+++ P1/tienda.js
@@ -1,40 +1,38 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 //-- ESpeficicación
 const PUERTO = 9090;
 
 //-- Crear el servidor
 const server = http.createServer((req, res) => {
-    
+
+   const filePath =  path.join("/home/alumnos/mccampos/LTAW/LTAW-Practicas/P1", "tiendita.html");
   //-- Indicamos que se ha recibido una petición
   console.log("Petición recibida!");
 
   //-- Cabecera que indica el tipo de datos del
   //-- cuerpo de la respuesta: Texto plano
-  res.setHeader('Content-Type', 'html');
+  //res.setHeader('Content-Type', 'text/html');
 
-  //-- Mensaje del cuerpo
-  res.write(`<!DOCTYPE html>
-  <html>
-  <head>
-      <meta charset='utf-8'>
-      <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-      <title>Page Title</title>
-      <meta name='viewport' content='width=device-width, initial-scale=1'>
-  </head>
-  <body>
-      <h1>Tiendita <\h1>
-  </body>
-  </html>`);
+  //-- Leer el archivo HTML y enviarlo como respuesta
+  //hacer el path
 
-  //-- Terminar la respuesta y enviarla
-  res.end();
+  fs.readFile(filePath,(err, data) => {
+    if (err) {
+      console.error(err);
+      res.writeHead(500);
+      res.end('Error interno del servidor');
+      
+    } else{
+        res.writeHead(200, {'Content-Type':'text/html'});
+        res.end(data);
+    }
+  });
 });
 
 //-- Activar el servidor: ¡Que empiece la fiesta!
 server.listen(PUERTO);
 
 console.log("Happy server activado!. Escuchando en puerto: " + PUERTO);
-
-
-// curl -vv 127.0.0.1:1234
