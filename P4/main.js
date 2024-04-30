ee02eef2fc14ab076be1383e57b6a1a71ffbdc32
MCcampos <mc.campos.2020@alumnos.urjc.es>
2024-04-30 14:22:44 +0200

    avances P4, html casi xd

--- P4/main.js
+++ P4/main.js
@@ -14,8 +14,44 @@ electron.app.on('ready', () => {
 
     //-- Crear la ventana principal de nuestra aplicación
     win = new electron.BrowserWindow({
-        width: 600,  //-- Anchura 
-        height: 400  //-- Altura
+        width: 600,   //-- Anchura 
+        height: 600,  //-- Altura
+
+        //-- Permitir que la ventana tenga ACCESO AL SISTEMA
+        webPreferences: {
+          nodeIntegration: true,
+          contextIsolation: false
+        }
     });
 
+  //-- En la parte superior se nos ha creado el menu
+  //-- por defecto
+  //-- Si lo queremos quitar, hay que añadir esta línea
+  //win.setMenuBarVisibility(false)
+
+  //-- Cargar contenido web en la ventana
+  //-- La ventana es en realidad.... ¡un navegador!
+  //win.loadURL('https://www.urjc.es/etsit');
+
+  //-- Cargar interfaz gráfica en HTML
+  win.loadFile("index.html");
+
+  //-- Esperar a que la página se cargue y se muestre
+  //-- y luego enviar el mensaje al proceso de renderizado para que 
+  //-- lo saque por la interfaz gráfica
+  win.on('ready-to-show', () => {
+    win.webContents.send('print', "MENSAJE ENVIADO DESDE PROCESO MAIN");
+  });
+
+  //-- Enviar un mensaje al proceso de renderizado para que lo saque
+  //-- por la interfaz gráfica
+  win.webContents.send('print', "MENSAJE ENVIADO DESDE PROCESO MAIN");
+
+});
+
+
+//-- Esperar a recibir los mensajes de botón apretado (Test) del proceso de 
+//-- renderizado. Al recibirlos se escribe una cadena en la consola
+electron.ipcMain.handle('test', (event, msg) => {
+  console.log("-> Mensaje: " + msg);
 });
