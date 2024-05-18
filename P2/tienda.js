4344698d39f697e8f95d63231e22b8c1e0f1e133
MCcampos <mc.campos.2020@alumnos.urjc.es>
2024-05-18 23:03:40 +0200

    purga

--- P2/tienda.js
+++ P2/tienda.js
@@ -1,41 +1,58 @@
+/*
+===============================================
+               importaciones
+===============================================
+*/
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const { cookie } = require('express/lib/response');
-//si hay definida cookie hacer lo que toque, si no, pues enseñar un _toastERROR que diga registrate
-const PUERTO = 8080;
-const FORMULARIO = fs.readFileSync('login.html', 'utf-8');
-const RESPUESTA = fs.readFileSync('gorrito1.html', 'utf-8'); //!VER 
+const url = require('url');
+
+
+
+/*
+===============================================
+               constantes
+===============================================
+*/
+const PUERTO = 8081;
+const LOGIN = fs.readFileSync('login.html', 'utf-8');
 const MAIN = fs.readFileSync('tienda.html', 'utf-8'); //!VER 
 const ERROR = fs.readFileSync('404.html');
-//----------json
 
+
+
+/*
+===============================================
+               json
+===============================================
+*/
 DATAJSON =  fs.readFileSync('tienda.json', 'utf-8')
 DATAJSON = JSON.parse(DATAJSON)
-cargarTienda(DATAJSON)
 
-// Array para almacenar los nombres de los productos
 let nombresProductos = [];
 
-// Iterar sobre el array de productos en DATAJSON
 for (let i = 0; i < DATAJSON.productos.length; i++) {
-    // Acceder al nombre del producto y agregarlo al array
     nombresProductos.push(DATAJSON.productos[i].nombre);
 }
+let productos = nombresProductos;
+console.log(productos);
 
-// Mostrar los nombres de los productos en la consola
-console.log(nombresProductos);
-//-------
+
+
+/*
+===============================================
+               server
+===============================================
+*/
 const server = http.createServer((req, res) => {
-    const url = req.url === '/' ? '/tienda.html' : req.url;
-    const filePath = path.join(__dirname, url);
+    const parsedUrl = url.parse(req.url, true);
+    const recurso = parsedUrl.pathname === '/' ? '/tienda.html' : parsedUrl.pathname;
+    const filePath = path.join(__dirname, recurso);
     const extension = path.extname(filePath);
-    const myURL = new URL(req.url, 'http://' + req.headers['host']);  
-    let recurso = myURL.pathname;
-    recurso = recurso.slice(1);
     
     
-
     let contentType = 'text/html';
 
     switch (extension) {
@@ -62,75 +79,9 @@ const server = http.createServer((req, res) => {
             contentType = 'image/gif';
         break;
     }
-    switch (recurso) {
-        case '':
-              console.log("Main page");
-              content = MAIN;
-             
-            break;
-
-        case 'productos':
-          console.log("Peticion de Productos!")
-          content_type = "application/json";
-    
-          //-- Leer los parámetros
-          let param1 = myURL.searchParams.get('param1');
-    
-          param1 = param1.toUpperCase();
-    
-          console.log("  Param: " +  param1);
-    
-          let result = [];
-    
-          for (let prod of nombresProductos) {
-    
-              //-- Pasar a mayúsculas
-              prodU = prod.toUpperCase();
-    
-              //-- Si el producto comienza por lo indicado en el parametro
-              //-- meter este producto en el array de resultados
-              if (prodU.startsWith(param1)) {
-                  result.push(prod);
-              }
-              
-          }
-         
-          console.log(result);
-          content = JSON.stringify(result);
-          
-          break;
-    
-          case 'cliente.js':
-              console.log("recurso: " + recurso);
-              recurso = recurso;
-              fs.readFile(recurso, 'utf-8', (err,data) => {
-                  if (err) {
-                      console.log("Error: " + err)
-                      return;
-                  } else {
-                    console.log('Todo ok')
-                    res.setHeader('Content-Type', 'application/javascript');
-                    res.write(data);
-                    res.end();
-                  }
-              });
-              
-            return;
-            break;
-
-            // default:
-            //     res.setHeader('Content-Type','text/html');
-            //     res.statusCode = 404;
-            //     res.write(ERROR);
-            //     res.end();
-            // return;
-              
-    }
-    
    
-    if (req.method === 'POST') {
+    if (req.method === 'POST' && parsedUrl.pathname === '/procesar') {
 
-        if (url == '/procesar'){
             console.log('\n\x1b[33m%s\x1b[0m', '---- ¡Iniciando sesión! ----\n');
             req.on('data', (content)=> {
                 content = (content.toString()).split("&")
@@ -167,12 +118,24 @@ const server = http.createServer((req, res) => {
                 }else{
                     console.log('error')
                 }
-            });
+        });
+    }
+
+
+    if (req.method === 'GET') {
+        if (parsedUrl.pathname === '/productos') {
+            console.log("Peticion de Productos!");
+            contentType = "application/json";
+            let param1 = parsedUrl.query.param1;
+            param1 = param1 ? param1.toUpperCase() : '';
+            console.log("  Param: " + param1);
+
+            let result = productos.filter(prod => prod.toUpperCase().startsWith(param1));
+            console.log(result);
+            res.setHeader('Content-Type', contentType);
+            res.end(JSON.stringify(result));
+            return;
         }
-        
-    // Manejar las solicitudes GET para otros recursos
-    } else {
-        
         fs.readFile(filePath, (err, content) => {
             if (err) {
                 if (err.code === 'ENOENT') {
@@ -185,7 +148,8 @@ const server = http.createServer((req, res) => {
                     res.end('Error interno del servidor: ' + err.code);
                 }
             } else {
-                 if (url == '/tienda.html'){
+              
+                 if (parsedUrl.pathname == '/tienda.html'){
                     cookies = getCookies(req)
                    if (cookies.userName === 'JK') {
                         procesarArchivoHTML('tienda.html', 0, DATAJSON);
@@ -206,14 +170,13 @@ const server = http.createServer((req, res) => {
                             res.end('Error interno del servidor al leer el archivo HTML');
                         }
                     });
-                    //data = manageMain(data, DATABASE ,cookies)  
-                    //OK(res,data)
+                   
                 }
-                if (url === '/gorrito1.html') {
+                if (parsedUrl.pathname === '/gorrito1.html') {
                     procesarArchivoHTML('gorrito1.html', 0, DATAJSON);
-                } else if (url === '/gorrito2.html') {
+                } else if (parsedUrl.pathname === '/gorrito2.html') {
                     procesarArchivoHTML('gorrito2.html', 1, DATAJSON);
-                } else if (url === '/gorrito3.html') {
+                } else if (parsedUrl.pathname === '/gorrito3.html') {
                     procesarArchivoHTML('gorrito3.html', 2, DATAJSON);
                 }
                 res.writeHead(200, { 'Content-Type': contentType });
@@ -242,29 +205,6 @@ server.listen(PUERTO, () => {
                FUNCIONES
 ===============================================
 */
-function comprar() {
-    res.setHeader('Set-Cookie', ["producto=" + content['userName']]);
-}
-
-
-function cargarTienda(DATAJSON) {
-    try {
-        console.log("Productos en la tienda: " + DATAJSON.productos.length);
-        let productos = [];
-        DATAJSON.productos.forEach(producto => {
-            productos.push({
-                nombre: producto.nombre,
-                descripcion: producto.descripcion
-            });
-            //console.log("Producto: " + producto.nombre);
-            //console.log("Descripción: " + producto.descripcion);
-        });
-        return productos;
-    } catch (error) {
-        console.error('Error al cargar el archivo JSON:', error);
-        return null;
-    }
-}
 
 
 function procesarArchivoHTML(nombreArchivo, posicionProducto, DATAJSON) {
